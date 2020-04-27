47c8c3a1ef169e0e2caf833dffab3de1629888a4
quentin_james <quentinqjs@gmail.com>
2020-04-27 23:44:44 +0200

    Updated class structure and added classes for treasure, water and grass tiles

--- tile.py
+++ tile.py
@@ -1,2 +1,14 @@
 class Tile:
     type = ["treasure", "water", "grass"]
+
+    # HTML representing hidden tile
+    element_not_visited = "<img alt=\"Hidden tile\" src=...">"
+
+class TreasureTile:
+    # code
+
+class WaterTile:
+    #code
+
+class GrassTile:
+    #code
