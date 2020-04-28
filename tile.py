bf96b3188c622e853841eb6de3a75c88c13afa99
quentin_james <quentinqjs@gmail.com>
2020-04-28 13:28:56 +0200

    Created classes for different tiles. Implemented getters for each tile type. Added png icons representing each tile

--- tile.py
+++ tile.py
@@ -1,14 +1,53 @@
+import enum
+
+
 class Tile:
-    type = ["treasure", "water", "grass"]
+    class Status(enum.Enum):
+        grass = 1
+        water = 2
+        treasure = 3
 
     # HTML representing hidden tile
-    element_not_visited = "<img alt=\"Hidden tile\" src=...">"
+    element_not_visited = "<img alt=\"Hidden tile\" src=...>"
 
-class TreasureTile:
-    # code
 
-class WaterTile:
-    #code
+class TreasureTile(Tile):
+    # HTML representing a treasure tile
+    element_treasure_tile = "<img alt=\"Treasure tile\" src=...>"
 
-class GrassTile:
-    #code
+    def get_html(self, visited):
+        if not visited:
+            return self.element_not_visited
+
+        return self.element_treasure_tile
+
+    def get_status(self):
+        return self.Status.treasure.name
+
+
+class WaterTile(Tile):
+    # HTML representing a water tile
+    element_water_tile = "<img alt=\"Water tile\" src=...>"
+
+    def get_html(self, visited):
+        if not visited:
+            return self.element_not_visited
+
+        return self.element_water_tile
+
+    def get_status(self):
+        return self.Status.water.name
+
+
+class GrassTile(Tile):
+    # HTML representing a grass tile
+    element_grass_tile = "<img alt=\"Grass tile\" src=...>"
+
+    def get_html(self, visited):
+        if not visited:
+            return self.element_not_visited
+
+        return self.element_grass_tile
+
+    def get_status(self):
+        return self.Status.grass.name
